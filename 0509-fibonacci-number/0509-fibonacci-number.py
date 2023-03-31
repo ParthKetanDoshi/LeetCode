7d197de3fc0996213424472e6a7b5c35954052c2
Parth Doshi <parthdoshi97@gmail.com>
2023-03-31 11:48:44 -0400

    Time: 19 ms (62.81%), Space: 13.2 MB (91.46%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,7 +4,9 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def fib_2(n):
-            if n<=1: return n
-            else: return fib_2(n-1) + fib_2(n-2)
-        return fib_2(n)
+        def fib_2(n, ht):
+            if n in ht: return ht[n]
+            else:
+                ht[n] = fib_2(n-1, ht) + fib_2(n-2, ht)
+                return ht[n]
+        return fib_2(n, {0:0, 1:1})
