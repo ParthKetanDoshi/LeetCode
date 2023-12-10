7ed2558067e57b7b6be2056df8ef00f2e4d3c2e8
Parth Doshi <parthdoshi97@gmail.com>
2023-12-10 15:03:16 -0500

    Time: 2085 ms (21.01%), Space: 56.4 MB (36.56%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.js
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.js
@@ -7,10 +7,11 @@ var findSubstring = function(s, words) {
     let result = [];
     const substringLength = words[0].length * words.length;
     const regex = new RegExp(".{" + words[0].length + "}", 'g');
+    let wordsChecked, checkWords;
     words = words.sort();
     for(let i = 0; i <= s.length - substringLength; i++) {
-        let checkWords = s.substring(i, i + substringLength).match(regex).sort();
-        let wordsChecked = true;
+        checkWords = s.substring(i, i + substringLength).match(regex).sort();
+        wordsChecked = true;
         for(let k = 0; k < words.length; k++) {
             if(words[k] !== checkWords[k]) {
                 wordsChecked = false;
