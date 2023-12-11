6e21eaeef7cc6545a3def6afc66166e83089bf9d
Parth Doshi <parthdoshi97@gmail.com>
2023-12-10 23:35:15 -0500

    Time: 67 ms (80.11%), Space: 49.9 MB (57.46%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -3,10 +3,10 @@
  * @return {number}
  */
 var minPartitions = function(n) {
-    let nArray = n.split("");
     let max = 0;
-    for(let i=0; i < nArray.length; i++) {
-        if(+nArray[i] > max) max = +nArray[i];
+    n = n.split("");
+    for(let i=0; i < n.length; i++) {
+        if(+n[i] > max) max = +n[i];
     }
     return max;
 };
