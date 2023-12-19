88b1343b7f4a299c3653bc30c98ae5986c55cec7
Parth Doshi <parthdoshi97@gmail.com>
2023-12-19 18:36:53 -0500

    Time: 49 ms (67.11%), Space: 41.8 MB (36.22%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -3,15 +3,12 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if(n <= 3) return n;
-    else {
-        let a = 2, res = 3;
-        while(n > 3) {
-            let temp = a + res;
-            a = res;
-            res = temp;
-            n--;
-        }
-        return res;
+    let a = 2, res = n > 3 ? 3 : n;
+    while(n > 3) {
+        let temp = a + res;
+        a = res;
+        res = temp;
+        n--;
     }
+    return res;
 };
