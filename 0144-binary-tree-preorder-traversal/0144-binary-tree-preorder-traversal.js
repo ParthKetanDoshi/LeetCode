94fb957f1e2dec1ee07fa2cc670392208bbbdc3a
Parth Doshi <parthdoshi97@gmail.com>
2023-12-21 13:02:26 -0500

    Time: 50 ms (61.91%), Space: 41.9 MB (72.91%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    const result = [];
+    
+    var traverse = function(node) {
+        if(!node) return;
+        
+        result.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+    }
+    
+    traverse(root);
+    
+    return result;
+};
