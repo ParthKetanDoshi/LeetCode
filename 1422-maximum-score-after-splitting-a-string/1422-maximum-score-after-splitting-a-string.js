24843f8e1961f2e90085986d9fdd53e90c368262
Parth Doshi <parthdoshi97@gmail.com>
2023-12-22 10:17:55 -0500

    Time: 55 ms (76.36%), Space: 42.3 MB (74.55%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
@@ -6,10 +6,10 @@ var maxScore = function(s) {
     let trailingOnes = s.length - s.split("").slice(1).join("").split("1").join("").length - 1;
     let leadingZeroes = 0;
     let maxScore = 0;
-    for(let firstPointer = 0, secondPointer = 1; secondPointer < s.length; firstPointer++, secondPointer++) {
-        if(s[firstPointer] === '0') leadingZeroes++;
+    for(let pointer = 0; pointer < s.length - 1; pointer++) {
+        if(s[pointer] === '0') leadingZeroes++;
         maxScore = maxScore > (leadingZeroes + trailingOnes) ? maxScore : (leadingZeroes + trailingOnes);
-        if(s[secondPointer] === '1') trailingOnes--;
+        if(s[pointer + 1] === '1') trailingOnes--;
     }
     return maxScore;
 };
