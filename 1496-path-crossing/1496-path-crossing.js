ef31069bb72866e3240eb6d324c3110894bb1335
Parth Doshi <parthdoshi97@gmail.com>
2023-12-23 03:37:28 -0500

    Time: 45 ms (89.21%), Space: 41.7 MB (90.65%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.js
+++ 1496-path-crossing/1496-path-crossing.js
@@ -4,14 +4,15 @@
  */
 var isPathCrossing = function(path) {
     let position = 1000010000, len=path.length;
+    const directions = new Map();
+    directions.set('N', 1);
+    directions.set('S', -1);
+    directions.set('E', 100000);
+    directions.set('W', -100000);
     const route = new Map();
     route.set(1000010000, 0);
     for(let i=0, position = 1000010000; i<len; i++) {
-        if(path[i] === 'N') position += 1;
-        else if(path[i] === 'S') position -= 1;
-        else if(path[i] === 'E') position += 100000;
-        else if(path[i] === 'W') position -= 100000;
-        
+        position += directions.get(path[i]);
         if(route.get(position) !== undefined) return true;
         route.set(position, 0);
     }
