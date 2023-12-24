e6cb22332764360b91e8aa83ce5a9576c169cb19
Parth Doshi <parthdoshi97@gmail.com>
2023-12-24 02:18:53 -0500

    Time: 58 ms (49.57%), Space: 42.3 MB (74.14%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.js
@@ -7,10 +7,8 @@ var minOperations = function(s) {
     for(i = 0; i < len; i += 2) {
         if(s[i] === '0') c1++;
         else c2++;
-    }
-    for(i = 1; i < len; i += 2) {
-        if(s[i] === '1') c1++;
-        else c2++;
+        if(s[i + 1] === '1') c1++;
+        else if(s[i + 1] === '0') c2++;
     }
     if(c1 < c2) return c1;
     else return c2;
