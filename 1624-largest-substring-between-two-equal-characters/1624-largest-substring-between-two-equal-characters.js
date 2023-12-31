9e6c9af0c8560c4876209df0cbbba8cc950e833c
Parth Doshi <parthdoshi97@gmail.com>
2023-12-30 23:51:35 -0500

    Time: 90 ms (10.00%), Space: 45.1 MB (10.00%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -3,9 +3,10 @@
  * @return {number}
  */
 var maxLengthBetweenEqualCharacters = function(s) {
-    let res = -1;
-    for(let i = 0; i < s.length; i++) {
-        let sArray = s.split(s[i]);
+    let res = -1, i = 0;
+    let sArray = [];
+    for(i = 0; i < s.length; i++) {
+        sArray = s.split(s[i]);
         if(sArray.length > 2) {
             sArray.splice(0, 1);
             sArray.splice(-1, 1);
