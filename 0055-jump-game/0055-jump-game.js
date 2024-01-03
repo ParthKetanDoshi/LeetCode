5d270624d87082d28f0d2f2e6f411907a68bd29f
Parth Doshi <parthdoshi97@gmail.com>
2024-01-03 15:11:27 -0500

    Time: 63 ms (64.71%), Space: 46.3 MB (56.22%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -3,13 +3,6 @@
  * @return {boolean}
  */
 var canJump = function(nums) {
-    /*
-    let i = 0;
-    while(i < nums.length) {
-        i += nums[i];
-    }
-    return i >= nums.length - 1;
-    */
     let goal = nums.length - 1;
     for(let i = nums.length - 1; i > -1; i--) if(i + nums[i] >= goal) goal = i;
     return goal === 0;
