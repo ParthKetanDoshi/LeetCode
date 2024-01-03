8d7c43bac7a86aeab9f73d8133c09e77f5338065
Parth Doshi <parthdoshi97@gmail.com>
2024-01-03 11:19:52 -0500

    Time: 116 ms (20.00%), Space: 52.3 MB (32.94%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
@@ -3,8 +3,8 @@
  * @return {number}
  */
 var numberOfBeams = function(bank) {
-    let prev = 0, beams = 0;
-    for(let i = 0; i < bank.length; i++) {
+    let beams = 0;
+    for(let i = 0, prev = 0; i < bank.length; i++) {
         let curr = bank[i].split('0').join('').length;
         if(curr !== 0) {
             if(prev !== 0) {
