a8c6578c9fdefd5fadd75b83b318909f662016a9
Parth Doshi <parthdoshi97@gmail.com>
2024-01-15 14:05:20 -0500

    Time: 51 ms (96.16%), Space: 17.5 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,7 +1,6 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices = sorted(prices)
-        diff = money - prices[0] - prices[1]
+        diff = money - sorted(prices)[0] - sorted(prices)[1]
         if diff >= 0:
             return diff
         else:
