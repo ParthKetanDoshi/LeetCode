27ea9130caaace0ee9d1321b4ada2b7c58cdff10
Parth Doshi <parthdoshi97@gmail.com>
2024-02-21 23:18:27 -0500

    Time: 609 ms (51.58%), Space: 21.5 MB (95.97%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,11 +1,10 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        trusted = defaultdict(int)
-        trusting = defaultdict(int)
+        delta = defaultdict(int)
         for src, dst in trust:
-            trusting[src] += 1
-            trusted[dst] += 1
+            delta[src] -= 1
+            delta[dst] += 1
         for i in range(1, n + 1):
-            if trusted[i] == n - 1 and trusting[i] == 0:
+            if delta[i] == n - 1:
                 return i
         return -1
