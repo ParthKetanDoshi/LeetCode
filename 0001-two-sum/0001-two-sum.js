d9091daa794c58f1f1208b79a725071005ef3700
Parth Doshi <parthdoshi97@gmail.com>
2024-08-23 19:59:16 -0400

    Time: 81 ms (48.04%), Space: 48.8 MB (99.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -3,11 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+var twoSum = function (nums, target) {
     let indexes = [];
     nums.some((num, index) => {
         let match = nums.indexOf(target - num, index + 1);
-        if(match != -1) {
+        if (match != -1) {
             indexes.push(index);
             indexes.push(match);
             return true;
