b29c0b429585a3761b52c5e4802f5ce87fc9c8d9
Parth Doshi <parthdoshi97@gmail.com>
2025-01-20 13:57:27 -0500

    Time: 13 ms (62.33%) | Memory: 59.9 MB (87.50%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const numset = new Set();
+    for(const num of nums) {
+        if(numset.has(num)) {
+            return true;
+        }
+        numset.add(num);
+    }
+    return false;
+};
